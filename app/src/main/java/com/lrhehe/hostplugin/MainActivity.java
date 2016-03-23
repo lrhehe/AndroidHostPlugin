c23bbeb7bb445baf4508c2260fd2373dd9153c08
lrhehe <lrhehe@gmail.com>
2016-03-23 11:33:19 +0800

    ADD: initial project

---
+++ app/src/main/java/com/lrhehe/hostplugin/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lrhehe.hostplugin;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
